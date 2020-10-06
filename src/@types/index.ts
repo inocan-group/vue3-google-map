3f29b845946c8be86dcb68c68d45c487b78cf9f7
Husam Ibrahim <huielbashir@gmail.com>
2020-10-06 14:34:07 +0200

    feat: remaining map options

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -4,6 +4,12 @@ export type GoogleMapsAPI = typeof google.maps
 export type IMap = google.maps.Map
 export type ILatLng = google.maps.LatLng
 export type IControlPosition = keyof typeof google.maps.ControlPosition
+export type IScaleControlStyle = google.maps.ScaleControlStyle
+export type IMapTypeControlOptions = google.maps.MapTypeControlOptions
+export type IMapTypeId = google.maps.MapTypeId
+export type IMapRestriction = google.maps.MapRestriction
+export type IStreetViewPanorama = google.maps.StreetViewPanorama
+export type IMapTypeStyle = google.maps.MapTypeStyle
 
 export type IMarker = google.maps.Marker
 export type IPolyline = google.maps.Polyline
--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -8,22 +8,58 @@
 import { defineComponent, PropType, ref, watch } from 'vue'
 import { loadNow } from 'connect-google-maps'
 import { useMap } from '@/composables/index'
-import { ILatLng, IControlPosition } from '@/@types/index'
+import {
+  ILatLng,
+  IControlPosition,
+  IScaleControlStyle,
+  IMapTypeControlOptions,
+  IMapTypeId,
+  IMapRestriction,
+  IStreetViewPanorama,
+  IMapTypeStyle,
+} from '@/@types/index'
 
 export default defineComponent({
   props: {
     apiKey: String,
+    backgroundColor: String,
     center: Object as PropType<ILatLng>,
-    fullscreen: Boolean,
-    fullscreenControlPosition: {
-      type: String as PropType<IControlPosition>,
-    },
-    streetView: Boolean,
-    streetViewControlPosition: {
-      type: String as PropType<IControlPosition>,
-    },
-    zoom: Number,
+    clickableIcons: Boolean,
     controlSize: Number,
+    disableDefaultUi: Boolean,
+    disableDoubleClickZoom: Boolean,
+    draggable: Boolean,
+    draggableCursor: String,
+    draggingCursor: String,
+    fullscreenControl: Boolean,
+    fullscreenControlPosition: String as PropType<IControlPosition>,
+    gestureHandling: String as PropType<'cooperative' | 'greedy' | 'none' | 'auto'>,
+    heading: Number,
+    keyboardShortcuts: Boolean,
+    mapTypeControl: Boolean,
+    mapTypeControlOptions: Object as PropType<IMapTypeControlOptions>,
+    mapTypeId: {
+      type: [Number, String] as PropType<IMapTypeId | string>,
+    },
+    maxZoom: Number,
+    minZoom: Number,
+    noClear: Boolean,
+    panControl: Boolean,
+    panControlPosition: String as PropType<IControlPosition>,
+    restriction: Object as PropType<IMapRestriction>,
+    rotateControl: Boolean,
+    rotateControlPosition: String as PropType<IControlPosition>,
+    scaleControl: Boolean,
+    scaleControlStyle: Number as PropType<IScaleControlStyle>,
+    scrollwheel: Boolean,
+    streetView: Object as PropType<IStreetViewPanorama>,
+    streetViewControl: Boolean,
+    streetViewControlPosition: String as PropType<IControlPosition>,
+    styles: Array as PropType<IMapTypeStyle[]>,
+    tilt: Number,
+    zoom: Number,
+    zoomControl: Boolean,
+    zoomControlPosition: String as PropType<IControlPosition>,
   },
   setup(props) {
     const mapRef = ref<HTMLElement | null>(null)
@@ -31,21 +67,66 @@ export default defineComponent({
     const { map, api } = useMap()
 
     const opts = () => ({
+      backgroundColor: props.backgroundColor,
       center: props.center,
-      fullscreenControl: props.fullscreen,
+      clickableIcons: props.clickableIcons,
+      controlSize: props.controlSize,
+      disableDefaultUi: props.disableDefaultUi,
+      disableDoubleClickZoom: props.disableDoubleClickZoom,
+      draggable: props.draggable,
+      draggableCursor: props.draggableCursor,
+      draggingCursor: props.draggingCursor,
+      fullscreenControl: props.fullscreenControl,
       fullscreenControlOptions: props.fullscreenControlPosition
         ? {
-            position: api.value?.ControlPosition[props.fullscreenControlPosition] || undefined,
+            position: api.value?.ControlPosition[props.fullscreenControlPosition],
           }
         : {},
-      streetViewControl: props.streetView,
+      gestureHandling: props.gestureHandling,
+      heading: props.heading,
+      keyboardShortcuts: props.keyboardShortcuts,
+      mapTypeControl: props.mapTypeControl,
+      mapTypeControlOptions: props.mapTypeControlOptions,
+      mapTypeId: props.mapTypeId,
+      maxZoom: props.maxZoom,
+      minZoom: props.minZoom,
+      noClear: props.noClear,
+      panControl: props.panControl,
+      panControlOptions: props.panControlPosition
+        ? {
+            position: api.value?.ControlPosition[props.panControlPosition],
+          }
+        : {},
+      restriction: props.restriction,
+      rotateControl: props.rotateControl,
+      rotateControlOptions: props.rotateControlPosition
+        ? {
+            position: api.value?.ControlPosition[props.rotateControlPosition],
+          }
+        : {},
+      scaleControl: props.scaleControl,
+      scaleControlOptions: props.scaleControlStyle
+        ? {
+            style: props.scaleControlStyle,
+          }
+        : {},
+      scrollwheel: props.scrollwheel,
+      streetView: props.streetView,
+      streetViewControl: props.streetViewControl,
       streetViewControlOptions: props.streetViewControlPosition
         ? {
-            position: api.value?.ControlPosition[props.streetViewControlPosition] || undefined,
+            position: api.value?.ControlPosition[props.streetViewControlPosition],
           }
         : {},
+      styles: props.styles,
+      tilt: props.tilt,
       zoom: props.zoom,
-      controlSize: props.controlSize,
+      zoomControl: props.zoomControl,
+      zoomControlOptions: props.zoomControlPosition
+        ? {
+            position: api.value?.ControlPosition[props.zoomControlPosition],
+          }
+        : {},
     })
 
     loadNow('places', props.apiKey).then(res => {
