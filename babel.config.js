c874c5015420521e58d534959275830667a35d0c
Husam Ibrahim <huielbashir@gmail.com>
2020-10-20 00:04:51 +0200

    chore: remove stale babel config file

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-const devPresets = ['@vue/babel-preset-app']
-const buildPresets = ['@babel/preset-env', '@babel/preset-typescript']
-module.exports = {
-  presets: process.env.NODE_ENV === 'development' ? devPresets : buildPresets,
-  plugins: [
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-  ],
-}
