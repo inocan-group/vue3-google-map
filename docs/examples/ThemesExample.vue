ed92986375b0b0de2ceae228d53df79893d202fb
Husam Ibrahim <huielbashir@gmail.com>
2020-10-29 17:23:33 +0200

    docs: update ThemesExample component

--- docs/examples/ThemesExample.vue
+++ docs/examples/ThemesExample.vue
@@ -1,21 +1,21 @@
 <template>
-  <GoogleMap :center="center" :theme="theme" :zoom="4" v-bind="$attrs" />
+  <GoogleMap :center="center" :styles="theme" :zoom="4" v-bind="$attrs" />
   <label for="theme">Theme</label>
   <select v-model="theme" id="theme">
     <option value="">-- None --</option>
-    <option v-for="theme in themes" :value="theme" :key="theme">{{ theme }}</option>
+    <option v-for="(theme, key) in themes" :value="theme" :key="key">{{ key }}</option>
   </select>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import { GoogleMap } from '/@/components/index'
+import * as themes from '/@/themes/index'
 
 export default defineComponent({
   components: { GoogleMap },
   setup() {
     const center = { lat: 39.50024, lng: -98.350891 }
-    const themes = ['aubergine', 'dark', 'grey', 'minimal', 'retro', 'roadways', 'roadwaysMinimal', 'ultraLight']
     const theme = ref('')
 
     return { center, themes, theme }
