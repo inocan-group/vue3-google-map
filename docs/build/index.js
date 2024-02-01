5daa0b0f9f08c221bb7347be5211876032e07d93
Husam Ibrahim <huielbashir@gmail.com>
2024-02-01 05:58:43 +0200

    chore: remove stale files

--- docs/build/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-module.exports = {
-  onPreBuild: () => {
-    fs.writeFileSync("./docs/env.js", `export const GOOGLE_API_KEY = '${process.env.GOOGLE_API_KEY}'`);
-  },
-};
