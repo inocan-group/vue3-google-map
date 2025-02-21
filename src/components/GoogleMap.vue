3f191ddf33133ec923e65c108d11a6bb3adc0a75
陈本杰 <benjie@addcn.com>
2025-02-21 18:51:40 +0800

    feat(map): Support CameraControlPosition

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -278,6 +278,8 @@ export default defineComponent({
         disableDefaultUI: props.disableDefaultUi,
       };
 
+      console.log({ ...options, ...otherOptions });
+
       return { ...options, ...otherOptions };
     };
 
