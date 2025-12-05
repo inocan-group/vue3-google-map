984215b5c191a58498e18b89fe7e76b39aef2379
Husam Elbashir <39692071+HusamElbashir@users.noreply.github.com>
2025-12-05 18:37:05 +0200

    chore: suppress TS warnings about explicit any in test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,7 @@ module.exports = {
       rules: {
         "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-explicit-any": "off",
       },
     },
   ],
